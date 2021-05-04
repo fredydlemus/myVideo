e11a0c440e5dad2a882165255bbe6ce415d2b231
fredyflemus <fredyflemus@gmail.com>
2021-05-03 22:05:11 -0600

    initializing hidden menu

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+function side(open){
+    if(open){
+        document.getElementById("side_menu").classList.add("show");
+    }else{
+        document.getElementById("side_menu").classList.remove("show");
+    }
+}
