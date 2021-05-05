a8bc21572fd4c6a5e49e4fe4b09b2d35f13554f2
fredyflemus <fredyflemus@gmail.com>
2021-05-04 23:01:10 -0600

    actualize hidden sidebar

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 function side(open){
     if(open){
         document.getElementById("side_menu").classList.add("show");
+        document.querySelector("body").classList.add("shadow")
     }else{
         document.getElementById("side_menu").classList.remove("show");
+        document.querySelector("body").classList.remove("shadow");
+        
     }
 }
