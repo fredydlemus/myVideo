90ff36a69bbefbdb60cc6c18fd79896fcb472a20
fredyflemus <fredyflemus@gmail.com>
2021-05-07 21:16:10 -0600

    fixed side menu

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,23 @@
+function cerrarMenu(){
+    document.getElementById("side_menu").classList.remove("show");
+    document.getElementById("darkWindow").classList.remove("shadow");
+    
+}
+
+function abrirMenu(){
+    document.getElementById("side_menu").classList.add("show");
+    document.getElementById("darkWindow").classList.add("shadow");
+    document.getElementById("darkWindow").addEventListener('click', this.cerrarMenu);
+    
+    
+}
+
 function side(open){
     if(open){
-        document.getElementById("side_menu").classList.add("show");
-        document.querySelector("body").classList.add("shadow")
+        abrirMenu();
     }else{
-        document.getElementById("side_menu").classList.remove("show");
-        document.querySelector("body").classList.remove("shadow");
+        cerrarMenu();
         
-    }
+        }
+        
 }
